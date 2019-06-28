7934859be1a88a84dd26879ef37e7ccd6a11836d
mathias <mathias.haegel@viacesi.fr>
2019-06-28 14:27:47 +0100

    Command + test

---
+++ APIDevice.test/ApiDeviceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace APIDevice.test
+{
+    [TestClass]
+    public class ApiDeviceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ APIDevice/Models/CommandModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIDevice.Models
+{
+    public class CommandModel
+    {
+        public string command;
+        public int id;
+        public string mac_address;
+    }
+}
